374d7309a647e67049360b4ce2673c124c8518b6
fzf <root@localhost.localdomain>
2015-02-24 21:16:36 +0800

    squid.sh

---
+++ oldsvn/linux/study/shell/squid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function squidinstall()
+{
+	#安装squid软件包
+	yum -y install squid
+	#squid 配置文件修改
+	/etc/squid/squid.conf
+	#启动squid服务
+	service squid start
+	#重新加载配置文件
+	squid -k reconfig
+}
+squidinstall
