294ff8bce2a45d0884760bfd1e29625df1a7975e
fzf <root@localhost.localdomain>
2015-09-08 11:38:47 +0800

    ipshell

---
+++ oldsvn/linux/study/shell/tcudump.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+function getipString()
+{
+	string='';
+	for ($i=0; $i<=$2; $i++)
+	do
+		{
+			ip=$1${i}
+			if ["$synum" -lt 40]
+			string=${string}'.'${ip}
+		}
+	done
+	return
+}
+function getTcpdump()
+{
+	ip1='110.75.120'
+	ip2='110.75.118'
+	ip3='112.124.131'
+	ip4='119.38.217'
+	ip5='42.120.64'
+	ip6='42.120.65'
+	ip7='42.120.66';
+	ip8='42.120.67';
+	ip9='140.205.128'
+	ip10='42.120.237'
+	ip11='42.120.165'
+	num1=24
+	num2=28
+	ipnum1=${num1}
+	ipnum2=${num1}
+	ipnum3=${num1}
+	ipnum4=${num1}
+	ipnum5=${num2}
+	ipnum6=${num2}
+	ipnum7=${num7}
+	ipnum8=${num8}
+	ipnum9=${num1}
+	ipnum10=${num1}
+	ipnum11=${num1}
+	for ()
+	do
+		{
+			
+		}
+	done
+	
+	
+}
